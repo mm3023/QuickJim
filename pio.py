4e9e3a18cf37ef684484bf5827ec7bc2bda95b3d
mm3023 <mikemoore227@gmail.com>
2024-11-21 15:30:56 -0500

    Update pio.py

--- pio.py
+++ pio.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 
+import sys
+import os # if you want this directory
+
+try:
+    sys.path.index('/dir/path') # Or os.getcwd() for this directory
+except ValueError:
+    sys.path.append('/dir/path')
+
+
+
 app = FastAPI()
 
 
