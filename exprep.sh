b95183b59b0337bbe0df4c932122252c527e3015
mm3023 <mikemoore227@gmail.com>
2024-11-21 15:17:11 -0500

    Update exprep.sh

--- exprep.sh
+++ exprep.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo which python3
+which python3
 ls
 cd /zenviroment/bin
 ls
-echo which python3
+which python3
 source activate
-echo which python3
+which python3
 
 #echo $PYTHONPATH
 #$PYTHONPATH=/zenviroment/bin
